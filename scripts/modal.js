2b60a5daa20a62f71d5773c05856c1b50a8d5eac
Marie-Carrere <marie-carrere@icloud.com>
2019-06-17 16:55:21 +0200

    :bug: Fix modal

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,4 +1,4 @@
-const modal = document.getElementById('simpleModal');
+const modal = document.getElementById('modal');
 
 /**
  * Modal opening
@@ -13,7 +13,7 @@ export const openModal = () => {
  * Modal closing
  */
 const closeBtn = document.getElementById('close-button');
-const closeModal = () => modal.style.display = 'none';
+export const closeModal = () => modal.style.display = 'none';
 closeBtn.addEventListener('click', closeModal);
 
 

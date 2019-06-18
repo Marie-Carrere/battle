a26759bd82cbce53b295b5c460095e0b0cd526d6
Marie-Carrere <marie-carrere@icloud.com>
2019-06-18 16:07:00 +0200

    :lipstick: Update UI of the starting page

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,3 +1,4 @@
+import { initGameBoard } from "./initGameBoard.js";
 
 const modal = document.getElementById('modal');
 
@@ -6,9 +7,7 @@ const modal = document.getElementById('modal');
  */
 export const openModal = () => {
     modal.style.display = 'block';
-    console.log('modal opened');
 }
-
 
 /**
  * Modal closing
@@ -17,10 +16,25 @@ const closeBtn = document.getElementById('close-button');
 export const closeModal = () => modal.style.display = 'none';
 closeBtn.addEventListener('click', closeModal);
 
-
 const closeOutsideModal = (event) => {
 		if(event.target == modal) {
     	modal.style.display = 'none';
     }
 }
 window.addEventListener('click', closeOutsideModal);
+
+/**
+ * Reload gameboard when clicking on yes 
+ */
+const reload = document.getElementById('label-yes');
+
+// export const reloadOnYes = () => initGameBoard();
+
+function reloadOnYes() {
+    // console.log('yay, it should reload now');
+    return initGameBoard(gameboard);
+}
+
+reload.addEventListener('click', reloadOnYes);
+
+
